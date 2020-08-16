a5173629cbe833f4c084b52eb2323d21b7374ccf
zzhalex <zzh001xx@hotmail.com>
2020-08-16 01:09:03 -0700

    add tailwind grid

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -57,5 +57,9 @@ export default function Dashboard() {
     return taskDiv(t);
   });
 
-  return <div className="Dashboard">{taskPart}</div>;
+  return (
+    <div className="Dashboard grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
+      {taskPart}
+    </div>
+  );
 }
